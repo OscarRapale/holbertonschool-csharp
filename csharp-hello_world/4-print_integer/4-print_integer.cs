0f373c6b03c74f823a93b53bb9bd2d0f4f353efc
OscarRapale <oscarrapale@gmail.com>
2024-12-04 14:33:00 -0400

    feat: Task 4 added

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,10 @@
+﻿namespace _4_print_integer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 972;
+        Console.WriteLine($"{number} Mission Street");
+    }
+}
