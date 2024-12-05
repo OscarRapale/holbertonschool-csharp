302b25b6855a4e881e834992bb4e31f0582e80c1
OscarRapale <oscarrapale@gmail.com>
2024-12-05 12:51:55 -0400

    feat: Task 6 added

--- csharp-hello_world/5-print_float/obj/Debug/netcoreapp2.1/5-print_float.AssemblyInfo.cs
+++ csharp-hello_world/5-print_float/obj/Debug/netcoreapp2.1/5-print_float.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("5-print_float")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f87417e9be6805f251eac42f571094965a0d184d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2b57f072a28598fbcf9210158828bc334fb33c")]
 [assembly: System.Reflection.AssemblyProductAttribute("5-print_float")]
 [assembly: System.Reflection.AssemblyTitleAttribute("5-print_float")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.Write($"{str}");
+        Console.Write($"{str}");
+        Console.Write($"{str}");
+        Console.WriteLine("");
+
+        string firstNineChars = str.Substring(0, 9);
+        Console.WriteLine($"{firstNineChars}");
+    }
+}
