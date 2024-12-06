a1bc3caa1ea45cda009db5a9ea79c599ee83d184
OscarRapale <oscarrapale@gmail.com>
2024-12-06 12:03:05 -0400

    feat: Task 4 added

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,12 @@
+﻿namespace _4_print_hexa;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int num = 0; num <= 98; num++)
+        {
+            Console.WriteLine($"{num} = 0x{num:X}");
+        }
+    }
+}
