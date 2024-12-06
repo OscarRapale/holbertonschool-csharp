17f3b85361f45b66ab0d04718d8546780e0ffa24
OscarRapale <oscarrapale@gmail.com>
2024-12-06 10:58:52 -0400

    feat: Task 2 added

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,11 @@
+﻿namespace _2_print_alphabet;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            Console.Write(letter);
+        }
+    }
+}
