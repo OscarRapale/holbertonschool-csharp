33164d56a59b05ad4f0c2b5b0c89955ba4144703
OscarRapale <oscarrapale@gmail.com>
2024-12-07 10:59:50 -0400

    feat: Task 6 added

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,22 @@
+﻿namespace _6_print_comb2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 8; i++)
+        {
+            for (int j = i+1; j <= 9; j++)
+            {
+                if (i == 8 && j == 9)
+                {
+                    Console.WriteLine($"{i}{j}");
+                }
+                else
+                {
+                    Console.Write($"{i}{j}, ");
+                }
+            }
+        }
+    }
+}
