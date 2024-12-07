c4244eff6abf1d4257469166d8d4ac5c2401da41
OscarRapale <oscarrapale@gmail.com>
2024-12-07 10:31:15 -0400

    feat: Task 5 added

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            string formattedNumber = $"{i:D2}";
+            if (i < 99)
+            {
+                Console.Write($"{formattedNumber}, ");
+            }
+            else
+            {
+                Console.Write($"{formattedNumber}\n");
+            }
+        }
+    }
+}
