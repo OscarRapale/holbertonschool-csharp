9dfce632a87fbb5b90f8839a714b1c241e3df085
OscarRapale <oscarrapale@gmail.com>
2024-12-07 18:36:21 -0400

    feat: Task 0 added

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = i;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(array[i]);
+            if (i < size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+
+        return array;
+    }
+}
---
+++ csharp-arrays_lists_dictionaries/README.md
@@ -0,0 +1,1 @@
+# C# Arrays, lists, dictionaries
