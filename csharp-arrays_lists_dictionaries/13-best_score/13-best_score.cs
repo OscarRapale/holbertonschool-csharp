b1c70c2bf4314d066a08734763f05b78a6d0d285
OscarRapale <oscarrapale@gmail.com>
2024-12-08 12:56:53 -0400

    feat: Task 13 added

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+
+        string bestKey = null;
+        int bestValue = -1;
+
+        foreach (KeyValuePair<string, int> kvp in myList)
+        {
+            if (kvp.Value > bestValue)
+            {
+                bestKey = kvp.Key;
+                bestValue = kvp.Value;
+            }
+        }
+        return bestKey;
+    }
+}
