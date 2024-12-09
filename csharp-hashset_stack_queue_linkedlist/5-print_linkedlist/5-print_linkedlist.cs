983d715d8156fbd2f4475e74641eb39287b1a037
OscarRapale <oscarrapale@gmail.com>
2024-12-09 18:32:40 -0400

    feat: Task 5 added

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+
+    {
+        if (size < 0)
+        {
+            return new LinkedList<int>();
+        }
+
+        LinkedList<int> linkedList = new LinkedList<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            linkedList.AddLast(i);
+        }
+
+        foreach (int item in linkedList)
+        {
+            Console.WriteLine(item);
+        }
+        return linkedList;
+    }
+}
