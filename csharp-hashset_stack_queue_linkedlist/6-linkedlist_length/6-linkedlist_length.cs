8a1dcc51427a85278e57721c371c63713e69b476
OscarRapale <oscarrapale@gmail.com>
2024-12-09 18:38:23 -0400

    feat: Task 6 added

---
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿namespace _6_linkedlist_length;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+
+        foreach (int item in myLList)
+        {
+            count++;
+        }
+        return count;
+    }
+}
