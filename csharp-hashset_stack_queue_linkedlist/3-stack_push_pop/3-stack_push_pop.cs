90adca1e29559f46fbcd376152c6aae1e82d90ff
OscarRapale <oscarrapale@gmail.com>
2024-12-09 18:10:58 -0400

    feat: Task 3 added

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+
+        if (aStack.Count > 0)
+        {
+            Console.WriteLine($"Top Item: {aStack.Peek()}");
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+
+        bool containsSearch = aStack.Contains(search);
+        Console.WriteLine($"Stack contains {search}: {containsSearch}");
+
+        if (containsSearch)
+        {
+            Stack<string> tempStack = new Stack<string>();
+            while (aStack.Count > 0)
+            {
+                string topItem = aStack.Pop();
+                if (topItem == search)
+                {
+                    break;
+                }
+                tempStack.Push(topItem);
+            }
+            while (tempStack.Count > 0)
+            {
+                aStack.Push(tempStack.Pop());
+            }
+        }
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
