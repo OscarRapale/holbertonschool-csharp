5224a9f20e3f62037f41a4efdf55d1bcd7336f2c
OscarRapale <oscarrapale@gmail.com>
2025-01-08 18:51:00 -0400

    feat: Task 2 added

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Public class Zombie</summary>
+    public class Zombie
+    {
+        /// <summary>Public field health</summary>
+        private int health;
+
+        /// <summary>Public constructor</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+         /// <summary>New public constructor with value parameter</summary>
+        public Zombie(int value)
+        {
+          if (value < 0)
+          {
+            throw new ArgumentException("Health must be greater than or equal to 0");
+          }  
+          this.health = value;
+        }
+
+    /// <summary>Public method for health</summary>
+    /// <returns>Health value</returns>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+
+}
