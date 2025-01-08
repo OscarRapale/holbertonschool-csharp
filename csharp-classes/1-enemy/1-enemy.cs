8b2755e82326842e8997765cc6912a35ad84079c
OscarRapale <oscarrapale@gmail.com>
2025-01-08 18:23:26 -0400

    feat: Task 1 added

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Enemies
+{
+    // Public class Zombie
+    public class Zombie
+    {
+        // Public field health
+        public int health;
+
+        // Public constructor
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+
+}
