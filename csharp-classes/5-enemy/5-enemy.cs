a6b85f63280d6b2b5e6ab317b044447072fbc9bb
OscarRapale <oscarrapale@gmail.com>
2025-01-08 19:26:56 -0400

    feat: Task 5 added

---
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Public class Zombie</summary>
+    public class Zombie
+    {
+        /// <summary>Private field health</summary>
+        private int health;
+
+        /// <summary>Private field name</summary>
+        private string name;
+
+        /// <summary>Public constructor</summary>
+        public Zombie()
+        {
+            this.health = 0;
+            this.name = "(No name)";
+        }
+
+        /// <summary>Public property Name</summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>New public constructor with value parameter</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+            this.name = "(No name)";
+        }
+
+        /// <summary>Public method for health</summary>
+        /// <returns>Health value</returns>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+        public override string ToString() => $"Zombie Name: {name} / Total Health: {health}";
+    }
+}
