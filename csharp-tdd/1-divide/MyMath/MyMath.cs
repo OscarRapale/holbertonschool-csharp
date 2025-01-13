f77216debabec00414fbf3cc9a54859a7665f528
OscarRapale <oscarrapale@gmail.com>
2025-01-12 22:04:33 -0400

    Task 1 added

---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Represents a matrix utility class.
+    /// </summary>
+    public class Matrix
+    {
+        /// <summary>
+        /// Divides all elements of a matrix.
+        /// </summary>
+        /// <param name="matrix">the matrix to divide</param>
+        /// <param name="num">the number to divide the matrix</param>
+        /// <returns>Divided matrix</returns>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            try
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = matrix[i, j] / num;
+                    }
+                }
+                return matrix;
+            }
+            catch (System.DivideByZeroException)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            catch (System.NullReferenceException)
+            {
+                return null;
+            }
+        }
+    }
+}
