cf48cc22863fa8627bd9499b349473ca0e0a2b74
OscarRapale <oscarrapale@gmail.com>
2025-01-12 22:37:14 -0400

    Task 4 added

---
+++ csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System;
+using Text;
+
+namespace Text.Tests
+{
+
+    public class Tests
+    {
+        [Test]
+        public void ReturnUnique()
+        {
+            string s = "Hello";
+            int result = Str.UniqueChar(s);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void NoUnique()
+        {
+            string s = "aaaaa";
+            int result = Str.UniqueChar(s);
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void EmptyString()
+        {
+            string s = "";
+            int result = Str.UniqueChar(s);
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void NullString()
+        {
+            string s = null;
+            int result = Str.UniqueChar(s);
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void ReturnUniqueChar()
+        {
+            string s = "a";
+            int result = Str.UniqueChar(s);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ReturnUniqueCharInMiddle()
+        {
+            string s = "abcba";
+            int result = Str.UniqueChar(s);
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ReturnUniqueCharAtEnd()
+        {
+            string s = "aaab";
+            int result = Str.UniqueChar(s);
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
---
+++ csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// Class to find the first unique character in a string
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// Method to find the first unique character in a string
+        /// </summary>
+        /// <param name="s">The string to search</param>
+        /// <returns>The index of the first unique character</returns>
+        public static int UniqueChar(string s)
+        {
+            if (s == null || s.Length == 0)
+                return -1;
+
+            int[] count = new int[256];
+            for (int i = 0; i < s.Length; i++)
+            {
+                count[s[i]]++;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (count[s[i]] == 1)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
