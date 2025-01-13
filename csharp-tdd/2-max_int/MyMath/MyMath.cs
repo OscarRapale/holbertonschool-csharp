9ff3aa9d0d0bdff59488bc8e07ef7c7787532bc2
OscarRapale <oscarrapale@gmail.com>
2025-01-12 22:14:59 -0400

    Task 2 added

---
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Represents a class that contains operations for integers.
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Returns the maximum integer from a list of integers.
+       /// </summary>
+        /// <param name="nums">List of integers</param>
+        /// <returns>Max int</returns>
+        public static int Max(List<int> nums)
+        {
+            // If list is null or empty, return 0
+            if (nums == null || nums.Count <= 0)
+            {
+                return 0;
+            }
+            int max = nums[0];
+
+            foreach (var value in nums)
+            {
+                max = Math.Max(max, value);
+            }
+            return max;
+        }
+    }
+}
