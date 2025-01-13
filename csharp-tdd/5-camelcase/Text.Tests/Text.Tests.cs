8748bad63594d27261cd58dbf3a90ae2c5547b05
OscarRapale <oscarrapale@gmail.com>
2025-01-12 22:43:23 -0400

    Task 5 added

---
+++ csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using Text;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class StrTests
+    {
+        [Test]
+        public void TestCamelCase()
+        {
+            Assert.That(Str.CamelCase("helloWorld"), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestCamelCaseEmptyString()
+        {
+            Assert.That(Str.CamelCase(""), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void TestCamelCaseNullString()
+        {
+            Assert.That(Str.CamelCase(null), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FullStatementCase()
+        {
+            Assert.That(Str.CamelCase("helloWhoAreYou"), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void TestCamelCaseSingleWord()
+        {
+            Assert.That(Str.CamelCase("hello"), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FullStatementWithSpacesCase()
+        {
+            Assert.That(Str.CamelCase("hello Who Are You"), Is.EqualTo(4));
+        }
+    }
+}
---
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// Class to count the number of words in a camel case string.
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// Count the number of words in a camel case string.
+        /// </summary>
+        /// <param name="s">The string to count the words in.</param>
+        /// <returns>The number of words in the string.</returns>
+        public static int CamelCase(string s)
+        {
+            if (s == null || s == "")
+            {
+                return 0;
+            }
+            int count = 1;
+            foreach (char c in s)
+            {
+                if (char.IsUpper(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
