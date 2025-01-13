0fa112cafa152926290adcaad9a969ed20858ae5
OscarRapale <oscarrapale@gmail.com>
2025-01-12 21:49:59 -0400

    Task 0 added

---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMath
+{
+    ///<summary>Provides mathematical operations.</summary>
+    public class Operations
+    {
+        ///<summary>Adds two integers and returns the result.</summary>
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
