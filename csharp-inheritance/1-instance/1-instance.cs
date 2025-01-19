fb689c7e615b5ff09d4cf906d1ec58e2d110add0
OscarRapale <oscarrapale@gmail.com>
2025-01-19 12:55:02 -0400

    feat: Task 1 added

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class to check if an object is an instance of an array.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Check if an object is an instance of an array.
+    /// </summary>
+    /// <param name="obj">Object to check.</param>
+    /// <returns>True if the object is an instance of an array, false otherwise.</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
