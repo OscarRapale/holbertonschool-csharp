2934e818785fe723dcb1fbbaaa7e8c4639ccdd2d
OscarRapale <oscarrapale@gmail.com>
2025-01-19 12:49:48 -0400

    feat: Task 0 added

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// The Obj class.
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Determines if the object is an integer.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>True if the object is an integer, false otherwise.</returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        return obj is int;
+    }
+}
